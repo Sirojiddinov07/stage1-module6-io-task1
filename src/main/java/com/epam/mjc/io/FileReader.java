7adb27e106ac3e6b2fd89b7017886e67622be7f4
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-01 13:35:18 +0500

    first commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder s = new StringBuilder();
+        byte[] buf = new byte[10];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            while (fis.read(buf) != -1) {
+                s.append(new String(buf));
+                buf = new byte[10];
+            }
+        } catch (IOException e) {
+            throw new TestFileIncorrectException(e.getMessage());
+        }
+        return getProfileFromRawString(s.toString());
+    }
+
+    private Profile getProfileFromRawString(String fileString) {
+        String[] strings = fileString.split("\n");
+        Profile profile = new Profile();
+        for (String str : strings) {
+            String[] words = str.split(": ");
+            switch (words[0]) {
+                case "Name":
+                    profile.setName(words[1]);
+                    break;
+                case "Age":
+                    profile.setAge(Integer.parseInt(words[1]));
+                    break;
+                case "Email":
+                    profile.setEmail(words[1]);
+                    break;
+                case "Phone":
+                    profile.setPhone(Long.parseLong(words[1]));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return profile;
     }
 }
---
+++ src/main/java/com/epam/mjc/io/TestFileIncorrectException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc.io;
+
+public class TestFileIncorrectException extends RuntimeException {
+    public TestFileIncorrectException(String message) {
+        super(message);
+    }
+
+}
